4f0375ee5b0fee0b9052c071bb504e871bf0b6ec
cdujardin4000 <51912624+cdujardin4000@users.noreply.github.com>
2022-08-24 21:15:08 +0200

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('HOSTNAME', 'server1932.mariadb.database.azure.com');
-define('USERNAME', 'azureuser@server1932');
-define('PASSWORD', 'Azure1932');
-define('DATABASE', 'biblioweb');
-
-
-
